751bc80ba76f451cd01b0c87b2efd9cf744e4677
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-11-25 15:38:31 +0530

    fixed versioning error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datatransformutils",
-    version="0.1.0",
+    version="0.1.1",
     description="Reusable data transformation utilities for AWS Glue using PySpark",
     author="Anup Kumar Mridha",
     author_email="anupkumarmridha.net@gmail.com",
